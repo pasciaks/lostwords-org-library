d8ba70c0617f1740b62273eb4c5b9a844427809c
Sheldon Pasciak <sheldon@pasciak.com>
2023-07-17 15:10:41 -0600

    Quick and dirty react use example

--- react-word-find-lostwords-org-libarary/src/App.js
+++ react-word-find-lostwords-org-libarary/src/App.js
@@ -1,29 +1,36 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+
 import "./App.css";
 
 const { createManyPuzzles } = require("@pasciaks/lostwords-org-library");
 
-let result = createManyPuzzles(1);
-
-console.log("Puzzles:", result);
+const generatePuzzle = () => {
+  return createManyPuzzles(1);
+};
 
 function App() {
+  const [value, setValue] = useState(generatePuzzle());
+
+  const convertString = (string_value) => {
+    let index = 0;
+    let currentLine = "";
+    if (!string_value) return;
+    for (var i = 0; i < 14; i++) {
+      for (var j = 0; j < 14; j++) {
+        currentLine += "&nbsp;" + string_value.substring(index, index + 1);
+        index++;
+      }
+      currentLine += "<BR />";
+    }
+    console.log(currentLine);
+    return currentLine;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        dangerouslySetInnerHTML={{ __html: convertString(value[0].p_data) }}
+      />
     </div>
   );
 }
