94c252dd3ed613eaf86982979b67c70594f03bca
Sheldon Pasciak <sheldon@pasciak.com>
2023-07-18 11:42:42 -0600

    React app improvements

--- react-word-find-lostwords-org-library/src/App.js
+++ react-word-find-lostwords-org-library/src/App.js
@@ -1,45 +1,32 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-// import logo from "./images/bmc_qr.png";
 import { Row, Col } from "react-bootstrap";
 
-// Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
-
 import "./index.css";
-
 import "./App.css";
 
 const lwLibrary = require("@pasciaks/lostwords-org-library");
 
 let consoletext = `
-
-888                       888                   
-888                       888                   
-888                       888                   
-888      .d88b.  .d8888b  888888                
-888     d88""88b 88K      888                   
-888     888  888 "Y8888b. 888                   
-888     Y88..88P      X88 Y88b.                 
-88888888 "Y88P"   88888P'  "Y888                
-                                                
-                                                
-                                                
-888       888                      888          
-888   o   888                      888          
-888  d8b  888                      888          
-888 d888b 888  .d88b.  888d888 .d88888 .d8888b  
-888d88888b888 d88""88b 888P"  d88" 888 88K      
-88888P Y88888 888  888 888    888  888 "Y8888b. 
-8888P   Y8888 Y88..88P 888    Y88b 888      X88 
-888P     Y888  "Y88P"  888     "Y88888  88888P' 
-                                                
-                                                
+888                       888    888       888                      888          
+888                       888    888   o   888                      888          
+888                       888    888  d8b  888                      888          
+888      .d88b.  .d8888b  888888 888 d888b 888  .d88b.  888d888 .d88888 .d8888b  
+888     d88""88b 88K      888    888d88888b888 d88""88b 888P"  d88" 888 88K      
+888     888  888 "Y8888b. 888    88888P Y88888 888  888 888    888  888 "Y8888b. 
+888     Y88..88P      X88 Y88b.  8888P   Y8888 Y88..88P 888    Y88b 888      X88 
+88888888 "Y88P"   88888P'  "Y888 888P     Y888  "Y88P"  888     "Y88888  88888P' 
+                                                                                 
+                                              
 `; // thanks to https://patorjk.com/software/taag/#p=display&f=Colossal&t=Lost%0AWords
 
-console.log(consoletext);
+let resultHtml;
+
+resultHtml = consoletext.replace(/(?:\r\n|\r|\n)/g, "<br>");
+
+resultHtml = resultHtml.replace(/\s/g, "&nbsp;");
 
 let puzzleSize = 8;
 
@@ -51,7 +38,8 @@ lwLibrary.setGlobalOptionValue("global_p_bends", 1);
 
 function App() {
   const [value, setValue] = useState("LOST,WORDS");
-  const [showAnswers, setShowAnswer] = useState(false);
+  const [showAnswers, setShowAnswers] = useState(0);
+  const [hintsOnly, setHintsOnly] = useState(true);
 
   // const [time, setTime] = useState("");
 
@@ -66,19 +54,14 @@ function App() {
         "https://random-word-api.herokuapp.com/word?number=5&length=6&lang=en"
       )
       .then(function (response) {
-        // handle success
         result = response.data.join(",");
       })
-      .catch(function (error) {
-        // handle error
+      .catch(function () {
         result = "LOST,WORDS";
       })
       .finally(function () {
-        // always executed
         return result;
       });
-
-    console.log(result);
 
     wordListToGenerateInPuzzle = result?.toUpperCase();
 
@@ -106,7 +89,6 @@ function App() {
     let extra = "font-weight:200;";
     let index = 0;
     let currentLine = "";
-    // let currentLineAscii = "";
 
     if (!string_value) return;
 
@@ -115,9 +97,14 @@ function App() {
         c = "red";
         extra = "font-weight:200;";
         let anyFound = false;
+        let showHowManyHintLetters = value[0].p_locations.length;
+
+        if (hintsOnly) {
+          showHowManyHintLetters = showAnswers;
+        }
         for (let ii = 0; ii < value[0].p_locations.length; ii++) {
           let howManyPoints = value[0].p_locations[ii].split(",");
-          for (let jj = 0; jj < howManyPoints.length; jj += 2) {
+          for (let jj = 0; jj < showHowManyHintLetters; jj += 2) {
             // eslint-disable-next-line
             if (howManyPoints[jj] == j + 1) {
               // eslint-disable-next-line
@@ -139,48 +126,45 @@ function App() {
           `&nbsp;<span style='${extra}color:${c}'>` +
           string_value.substring(index, index + 1) +
           "</span>";
-
-        // currentLineAscii += "." + string_value.substring(index, index + 1);
-
         index++;
       }
       currentLine += "<BR />";
-      // currentLineAscii += "\n";
     }
-    // console.log(currentLineAscii);
+
     return currentLine;
   };
 
-  useEffect(() => {
-    // If the below code is enabled, will refresh rerender ever 1 second
-    // const intervalId = setInterval(() => {
-    //   setTime(new Date().toLocaleString());
-    // }, 1000);
-    // return () => {
-    //   clearInterval(intervalId);
-    // };
-  }, [value]);
+  useEffect(() => {}, [value, showAnswers]);
 
   return (
     <>
       <div className="App">
         <div className="container">
-          {/* <img src={logo} width={100} height={100} alt="buy me a coffee" /> */}
-
+          <Row>
+            <p
+              className={"fs-6"}
+              dangerouslySetInnerHTML={{ __html: resultHtml }}
+            />
+          </Row>
           <Row>
             <Col>
               <button onClick={getWords}>Create</button>
-            </Col>
-            <Col>
+              {/* @todo - convert to function call and not inline button click */}
               <button
                 onClick={() => {
-                  setShowAnswer(!showAnswers);
+                  let cv = showAnswers;
+                  if (cv === 0) {
+                    setShowAnswers(1);
+                  } else if (cv === 1) {
+                    setShowAnswers(99);
+                  } else {
+                    setShowAnswers(0);
+                  }
                 }}
               >
                 Answers
               </button>
-            </Col>
-            <Col>
+              {/* @todo - convert to function call and not inline button click */}
               <button
                 onClick={() => {
                   puzzleSize = 8;
@@ -193,9 +177,7 @@ function App() {
               >
                 8
               </button>
-            </Col>
-            <Col>
-              {" "}
+              {/* @todo - convert to function call and not inline button click */}
               <button
                 onClick={() => {
                   puzzleSize = 14;
@@ -209,18 +191,9 @@ function App() {
                 14
               </button>
             </Col>
-            <Col></Col>
-          </Row>
-          <Row>
-            <Col>
-              <br />
-              <div dangerouslySetInnerHTML={{ __html: value[0].p_words }} />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <br />
 
+            <Col>
+              <div dangerouslySetInnerHTML={{ __html: value[0].p_words }} />
               <div
                 dangerouslySetInnerHTML={{
                   __html: convertString(value[0].p_data),
